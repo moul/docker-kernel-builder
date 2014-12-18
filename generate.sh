a60313a992543b5da3d83fcdc845cb9c2e5c9746
Manfred Touron <m@42.am>
2014-12-19 00:37:20 +0100

    Removed base suffix, fixed variable expansion missing

--- generate.sh
+++ generate.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-for version in 3.17 3.18; do
-    mkdir ${version}-base
-    cat <<EOF > ${version}-base/Dockerfile
+VERSIONS="3.17 3.18"
+
+for version in $VERSIONS; do
+    mkdir -p ${version}
+    cat <<EOF > ${version}/Dockerfile
 FROM ubuntu:latest
 
 ENV KVER ${version}
 RUN apt-get update && apt-get -y -q upgrade && apt-get clean
-RUN mkdir -p /usr/src/linux && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-$KVER.tar.xz | tar -C /usr/src/linux/ -xf -
+RUN mkdir -p /usr/src/linux && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-\$KVER.tar.xz | tar -C /usr/src/linux/ -xf -
 WORKDIR /usr/src/linux
 EOF
 
-    mkdir ${version}-cross-arm
+    mkdir -p ${version}-cross-arm
     cat <<EOF > ${version}-cross-arm/Dockerfile
 FROM ubuntu:latest
 
-ENV KVER ${version}
+ENV KVER $version
 RUN apt-get update && apt-get -y -q upgrade && apt-get clean
-RUN mkdir -p /usr/src/linux && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-$KVER.tar.xz | tar -C /usr/src/linux/ -xf -
+RUN mkdir -p /usr/src/linux && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-\$KVER.tar.xz | tar -C /usr/src/linux/ -xf -
 RUN apt-get -y -q install gcc-arm-none-eabi
 ENV ARCH arm
 WORKDIR /usr/src/linux
