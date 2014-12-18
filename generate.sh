9d2c1544979e538c6f46a7c30bede178b91a5383
Manfred Touron <m@42.am>
2014-12-19 00:53:03 +0100

    Added xz-utils dependency

--- generate.sh
+++ generate.sh
@@ -9,20 +9,14 @@ for version in $VERSIONS; do
 FROM ubuntu:latest
 
 ENV KVER ${version}
-RUN apt-get update && apt-get -y -q upgrade && apt-get -y -q install wget && apt-get clean
+RUN apt-get update && apt-get -y -q upgrade && apt-get -y -q install wget xz-utils && apt-get clean
 RUN mkdir -p /usr/src/linux && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-\$KVER.tar.xz | tar -C /usr/src/linux/ -xf -
 WORKDIR /usr/src/linux
 EOF
 
     mkdir -p ${version}-cross-arm
-    cat <<EOF > ${version}-cross-arm/Dockerfile
-# ${version}
-FROM ubuntu:latest
-
-ENV KVER $version
-RUN apt-get update && apt-get -y -q upgrade && apt-get -y -q install wget && apt-get clean
-RUN mkdir -p /usr/src/linux && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-\$KVER.tar.xz | tar -C /usr/src/linux/ -xf -
-WORKDIR /usr/src/linux
+    cp ${version}/Dockerfile ${version}-cross-arm/Dockerfile
+    cat <<EOF >> ${version}-cross-arm/Dockerfile
 
 # cross-arm specific
 RUN apt-get -y -q install gcc-arm-none-eabi
