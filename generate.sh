0cdddc8b5c760c3048001f3259d50f115a62099c
Manfred Touron <m@42.am>
2014-12-19 11:37:02 +0100

    exposing armv5 and armv6

--- generate.sh
+++ generate.sh
@@ -11,7 +11,7 @@ FROM ubuntu:vivid
 ENV KVER ${version}
 RUN apt-get update \
  && apt-get -y -q upgrade \
- && apt-get -y -q install libncurses-dev wget xz-utils \
+ && apt-get -y -q install libncurses-dev wget xz-utils build-essential \
  && apt-get clean
 RUN mkdir -p /usr/src/ \
  && wget -q https://kernel.org/pub/linux/kernel/v3.x/linux-\$KVER.tar.xz -O - | tar -C /usr/src/ -xJf - \
@@ -19,13 +19,24 @@ RUN mkdir -p /usr/src/ \
 WORKDIR /usr/src/linux
 EOF
 
-    mkdir -p ${version}-cross-arm
-    cp ${version}/Dockerfile ${version}-cross-arm/Dockerfile
-    cat <<EOF >> ${version}-cross-arm/Dockerfile
+    mkdir -p ${version}-cross-armhf
+    cp ${version}/Dockerfile ${version}-cross-armhf/Dockerfile
+    cat <<EOF >> ${version}-cross-armhf/Dockerfile
 
-# cross-arm specific
-RUN apt-get -y -q install gcc-arm-none-eabi
+# cross-armhf specific
+RUN apt-get -y -q install gcc-4.9-arm-linux-gnueabihf
 ENV ARCH arm
+ENV CROSS_COMPILE arm-linux-gnueabihf-
+EOF
+
+    mkdir -p ${version}-cross-armel
+    cp ${version}/Dockerfile ${version}-cross-armel/Dockerfile
+    cat <<EOF >> ${version}-cross-armel/Dockerfile
+
+# cross-armel specific
+RUN apt-get -y -q install gccgo-4.7-arm-linux-gnueabi
+ENV ARCH arm
+ENV CROSS_COMPILE arm-linux-gnueabi-
 EOF
 
 done
